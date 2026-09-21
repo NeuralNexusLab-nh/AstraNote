7a4b09e17de4d5538939e10da04fd12d135c02db
NeuralNexusLab <neuralnexuslab@hotmail.com>
2026-09-21 09:58:45 +0800

    Log incoming request metadata

--- server.js
+++ server.js
@@ -1717,6 +1717,12 @@ async function accountPayload(username) {
 
 app.set("trust proxy", 1);
 app.disable("x-powered-by");
+app.use((req, res, next) => {
+  console.log(
+    `IP: ${req.ip}, method: ${req.method}, path: ${req.path}, UA: ${req.headers["user-agent"]}`,
+  );
+  next();
+});
 app.use(
   helmet({
     frameguard: false,
